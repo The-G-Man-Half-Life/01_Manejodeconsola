23085510d2536e0342fa3ece1a743ccba338eafa
The-G-Man-Half-Life <mateomoospina@gmail.com>
2024-04-18 21:32:08 -0500

    ejercicios terminados

--- 03_manejoDeNumeros/src/js/ejercicios.js
+++ 03_manejoDeNumeros/src/js/ejercicios.js
@@ -257,222 +257,338 @@ console.groupEnd();
 
 console.groupCollapsed("Ejercicio 42");
 // Ejercicio 42
-for(let i=0; i)
+// let fact7 = 1
+// for(let i=1; i<=7; i++){
+//     fact7 *=i
+// }
+// console.log(fact7)
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 43");
 // Ejercicio 43
+// console.log(Math.round(7.9))
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 44");
 // Ejercicio 44
+// console.log(15*2)
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 45");
 // Ejercicio 45
+// console.debug(6*3)
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 46");
 // Ejercicio 46
+// console.warn(Math.pow(8,2))
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 47");
 // Ejercicio 47
+// console.error(Math.pow(4,3))
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 48");
 // Ejercicio 48
+// let naturales15 = 15
+// let resultado = 0
+// for(i=naturales15; i>=1; i--){
+//     resultado += i
+// }
+// console.error(resultado)
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 49");
 // Ejercicio 49
+// console.log(100-55)
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 50");
 // Ejercicio 50
+// console.error(6*8)
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 51");
 // Ejercicio 51
+// console.error(225/15)
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 52");
 // Ejercicio 52
+// console.error(20%7)
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 53");
 // Ejercicio 53
+// console.warn(Math.floor(Math.random()*(50-40+1)+40))
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 54");
 // Ejercicio 54
+// console.warn(Math.pow(3,4))
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 55");
 // Ejercicio 55
+// console.error(Math.sqrt(169,2))
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 56");
 // Ejercicio 56
+// let celsius = 80
+// let farenheit = (celsius*1.8)+32
+// console.log(`${celsius} grados celsius es igual a ${farenheit} grados farenheit`)
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 57");
 // Ejercicio 57
+// console.warn("El area de su rectangulo es:", 8*12)
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 58");
 // Ejercicio 58
+// let perimetroCirculo8 = 2*(Math.PI)*8
+// console.log("El perimetro de un circulo con radio 8 es:", perimetroCirculo8)
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 59");
 // Ejercicio 59
+// console.info("El volumen del cubo con lado 5 es:", 5*5*5)
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 60");
 // Ejercicio 60
+// let diagonalMayor = 16
+// let diagonalMenor = 12
+// let areaRombo = ((diagonalMayor*diagonalMenor)/(2))
+// console.log(`El area de un rombo con diagonales ${diagonalMayor} y ${diagonalMenor} es: ${areaRombo}`)
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 61");
 // Ejercicio 61
+// let baseMenor = 8
+// let baseMayor = 12
+// let altura = 6
+// let areaTrapecio = (((baseMayor+baseMenor)/(2))*(altura))
+// console.log(areaTrapecio)
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 62");
 // Ejercicio 62
+// for (let i = 0; i<=11; i++){
+//     let multiplicacion5 = i*5;
+//     console.log(`${i}. 5*${i} es: ${multiplicacion5}`);
+// }
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 63");
 // Ejercicio 63
+// let arrayNumerico = [25,30,35]
+// console.log(Math.max(...arrayNumerico))
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 64");
 // Ejercicio 64
+// let arrayNumerico = [80,95,110]
+// console.warn(Math.min(...arrayNumerico))
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 65");
 // Ejercicio 65
+// let suma = 8+10+14
+// let promedio = suma/3
+// console.error(promedio)
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 66");
 // Ejercicio 66
+// let fact8 = 1
+
+// for (let i = 1; i < 8; i++) {
+//     fact8 *= i
+// }
+// console.log(fact8)
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 67");
 // Ejercicio 67
+// console.log(Math.round(9.2))
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 68");
 // Ejercicio 68
+// console.warn(18*2)
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 69");
 // Ejercicio 69
+// console.error(7*3)
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 70");
 // Ejercicio 70
+// console.error(Math.pow(10,2))
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 71");
 // Ejercicio 71
+// console.log(Math.pow(5,3))
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 72");
 // Ejercicio 72
+// let numbers1To20 = 20
+// let resultado = 0
+// for (let i =numbers1To20; i>0; i--){
+//     resultado +=i
+// }
+// console.log(resultado)
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 73");
 // Ejercicio 73
+// console.log(200-75)
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 74");
 // Ejercicio 74
+// console.info(9*11)
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 75");
 // Ejercicio 75
+// console.log(300/25)
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 76");
 // Ejercicio 76
+// console.info(30%9)
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 77");
 // Ejercicio 77
+// console.log(Math.floor(Math.random()*(70-60+1)+60))
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 78");
 // Ejercicio 78
+// console.log(Math.pow(4,5))
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 79");
 // Ejercicio 79
+// console.info(Math.sqrt(196,2))
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 80");
 // Ejercicio 80
+// let celsius = 120
+// let farenheit = (celsius*1.8)+32
+// console.log(`${celsius} grados celsius es igual a ${farenheit} grados farenheit`)
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 81");
 // Ejercicio 81
+// console.log(10*15)
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 82");
 // Ejercicio 82
+// let perimetroCirculo10 = 2*(Math.PI)*10
+// console.log("El perimetro de un circulo con radio 10 es:", perimetroCirculo10)
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 83");
 // Ejercicio 83
+// console.log(6*6*6)
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 84");
 // Ejercicio 84
+// let diagonalMayor = 18
+// let diagonalMenor = 14
+// let areaRombo = ((diagonalMayor*diagonalMenor)/(2))
+// console.log(`El area de un rombo con diagonales ${diagonalMayor} y ${diagonalMenor} es: ${areaRombo}`)
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 85");
 // Ejercicio 85
+// let baseMenor = 10
+// let baseMayor = 15
+// let altura = 8
+// let areaTrapecio = (((baseMayor+baseMenor)/(2))*(altura))
+// console.log(areaTrapecio)
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 86");
 // Ejercicio 86
+// for (let i = 0; i<=11; i++){
+//     let multiplicacion10 = i*10;
+//     console.log(`${i}. 10*${i} es: ${multiplicacion10}`);
+// }
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 87");
 // Ejercicio 87
+// let arrayNumerico = [35,40,45]
+// console.log(Math.max(...arrayNumerico))
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 88");
 // Ejercicio 88
+// let arrayNumerico = [90,105.120]
+// console.log(Math.min(...arrayNumerico))
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 89");
 // Ejercicio 89
+// let suma = 9+11+15
+// let promedio = suma/3
+// console.error(promedio)
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 90");
 // Ejercicio 90
+// let fact9 = 1
+
+// for (let i = 1; i < 10; i++) {
+//     fact9 *= i
+// }
+// console.log(fact9)
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 91");
 // Ejercicio 91
+// console.log(Math.round(10.8))
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 92");
 // Ejercicio 92
+// console.log(21*2)
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 93");
 // Ejercicio 93
+// console.log(8*3)
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 94");
 // Ejercicio 94
+// console.error(Math.pow(12,2))
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 95");
 // Ejercicio 95
+// console.log(Math.pow(6,3))
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 96");
 // Ejercicio 96
+// let sumaPrimeros25Numeros = 0
+// for(let i = 0; i<=25; i++){
+//     sumaPrimeros25Numeros +=i
+// }
+// console.log(sumaPrimeros25Numeros)
 console.groupEnd();
 
