6ac8df75c65197dad513dac29b51cdd6ae350577
The-G-Man-Half-Life <mateomoospina@gmail.com>
2024-04-19 13:27:48 -0500

    trabajo terminado

---
+++ 04_manejoDeOperadores/src/js/ejercicios.js
@@ -0,0 +1,150 @@
+console.groupCollapsed("Ejercicio 1");
+// Ejercicio 1
+console.log("¿Es 35 mayor que 25?",35>25)
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 2");
+// Ejercicio 2
+console.log("¿Es 45 menor que 30?",45<30)
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 3");
+// Ejercicio 3
+console.warn("¿Son 50 y 50 iguales?",50==50)
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 4");
+// Ejercicio 4
+console.warn("¿Es 40 estrictamente igual a 40 convertido a numero",40===Number(40))
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 5");
+// Ejercicio 5
+console.error("¿Son 'apple' y 'orange' diferentes?", "apple"!=="orange")
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 6");
+// Ejercicio 6
+console.warn("¿Es 'apple' estrictamente diferente de 'Apple'?", "apple"!=="Apple")
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 7");
+// Ejercicio 7
+let variableindefinida =undefined
+console.log("La variable de la variable indefinida es:",variableindefinida);
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 8");
+// Ejercicio 8
+console.log("La variable indefinida es de tipo:", typeof(variableindefinida))
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 9");
+// Ejercicio 9
+console.warn("¿Es null igual a undefined ?:",null==undefined)
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 10");
+// Ejercicio 10
+console.error("¿Es null y undefined estrictamente iguales ?:",null===undefined)
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 11");
+// Ejercicio 11
+console.log("¿Es 2.75 mayor que 2.5 ?:", 2.75>2.5)
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 12");
+// Ejercicio 12
+console.log("¿Es 2.75 mayor que 2.75 ?:",2.75>2.75)
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 13");
+// Ejercicio 13
+console.error("¿Es 2.75 mayor o igual a 2.75 ?:", 2.75>=2.75)
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 14");
+// Ejercicio 14
+console.debug("¿Es 2.75 menor o igual que 2.75 ?:",2.75<=2.75)
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 15");
+// Ejercicio 15
+console.warn("¿Es 2.75 un numero mayor que 2.8 ?:",2.75>2.8)
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 16");
+// Ejercicio 16
+console.error("¿Es 2.75  un numero menor a 2.7 ?:", 2.75<2.7)
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 17");
+// Ejercicio 17
+console.debug("¿Es el nombre John diferente de john ?:", "John"!="john")
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 18");
+// Ejercicio 18
+console.error("¿Es el nombre John diferente Jon","John ?:"!="Jon")
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 19");
+// Ejercicio 19
+console.log("¿Es la edad 30 estrictamente diferente de '30'","30 ?:"!==30)
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 20");
+// Ejercicio 20
+console.error("¿Cuál es el resultado de desplazar a la derecha 25 dos veces en bits ?:",25>>2)
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 21");
+// Ejercicio 21
+console.log("¿Cuál es el resultado de desplazar a la izquierda 8 tres veces en bits ?:",8<<3)
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 22");
+// Ejercicio 22
+console.error("¿Son 15 y 15 iguales ?:", 15==15)
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 23");
+// Ejercicio 23
+console.debug("¿Son 20 y 30 diferentes ?:",20!=30)
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 24");
+// Ejercicio 24
+console.error("¿Es '15' mayor que 30 ?:","15">30)
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 25");
+// Ejercicio 25
+console.warn("¿Es 20 menor que 40 ?:", 20<40)
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 26");
+// Ejercicio 26
+console.error("¿Es 100 mayor o igual a '99' ?:", 100>=99)
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 27");
+// Ejercicio 27
+console.log("¿Es 500 estrictamente igual a '500' ?:",500==="500")
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 28");
+// Ejercicio 28
+console.debug("¿Es 750 estrictamente igual a '750' ?:", 750==="750")
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 29");
+// Ejercicio 29
+console.error("¿Es 5 estrictamente diferente de '5' ?:",5!=="5")
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 30");
+// Ejercicio 30
+console.log("¿Es 3 mayor que 5 y 4 menor que 6 ?:",(3>5)&&(4<6))
+console.groupEnd();
