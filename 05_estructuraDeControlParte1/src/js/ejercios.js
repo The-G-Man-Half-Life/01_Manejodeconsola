0be0495297dae1fadd5aa486aeb8d9209a8b2d21
The-G-Man-Half-Life <mateomoospina@gmail.com>
2024-04-19 15:06:58 -0500

    trabajo hecho parcialmente

---
+++ 05_estructuraDeControlParte1/src/js/ejercios.js
@@ -0,0 +1,265 @@
+console.groupCollapsed("Ejercicio 1");
+// Ejercicio 1
+// let numberRandom2 = (Math.floor(Math.random() * (5 + 5 + 1) - 5))
+// if (numberRandom2 >= 1) {
+//     console.log("El numero:", numberRandom2, "es positivo")
+// }
+// else if (numberRandom2 < 0) {
+//     console.log("El numero", numberRandom2, "es negativo")
+// }
+// else {
+//     console.log("El numero", numberRandom2, "es neutro")
+// }
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 2");
+// Ejercicio 2
+// let numberRandom3 = (Math.floor(Math.random() * (5 - (-5) + 1) - 5))
+// if (numberRandom2 >= 1) {
+//     console.log("El numero:", numberRandom3, "es positivo")
+// }
+// else if (numberRandom2 < 0) {
+//     console.log("El numero", numberRandom3, "es negativo")
+// }
+// else {
+//     console.log("El numero", numberRandom3, "es neutro")
+// }
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 3");
+// Ejercicio 3
+// let numberRandom = (Math.floor(Math.random() * (5 - (-5) + 1) - 5))
+// if (numberRandom % 2 == 0) {
+//     console.log("El numero", numberRandom, "es par")
+// }
+// else {
+//     console.log("El numero", numberRandom, "es impar")
+// }
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 4");
+// Ejercicio 4
+// let numberRandom1 = (Math.floor(Math.random() * (5 - (-5) + 1) - 5))
+// if (numberRandom % 2 == 0) {
+//     console.log("El numero", numberRandom1, "es par")
+// }
+// else {
+//     console.log("El numero", numberRandom1, "es impar")
+// }
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 5");
+// Ejercicio 5
+// let numberRandom4 = (Math.floor(Math.random() * (100-1) + 1) - 1)
+// if (numberRandom4%5 == 0){
+//     console.log("El numero:",numberRandom4, "es multiplo de 5")
+// }
+// else{
+//     console.log("El numero:",numberRandom4, "no es multiplo de 5")
+// }
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 6");
+// Ejercicio 6
+// let numberRandom5 = (Math.floor(Math.random() * (100-1) + 1) - 1)
+// if (numberRandom5%3 == 0){
+//     console.log("El numero:",numberRandom5, "es multiplo de 3")
+// }
+// else{
+//     console.log("El numero:",numberRandom5, "no es multiplo de 3")
+// }
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 7");
+// Ejercicio 7
+// let numberRandom6 = Math.floor(Math.random()*(500 -1 + 1)+1)
+// if (numberRandom6 >100){
+//     console.warn("El numero", numberRandom6, "es mayor que 100")
+// }
+// else{
+//     console.warn("El numero", numberRandom6, "es menor que 100")
+// }
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 8");
+// Ejercicio 8
+// let numberRandom7 = Math.floor(Math.random() * (1 + 500 + 1) - 500)
+// if (numberRandom7 < -50) {
+//     console.warn("El numero", numberRandom7, "es menor que -50")
+// }
+// else {
+//     console.warn("El numero", numberRandom7, "es mayor que -50")
+// }
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 9");
+// Ejercicio 9
+// let numberRandom8 = Math.floor(Math.random() * (100 -1 + 1) +1)
+// if ((numberRandom8>=20) && (numberRandom8<=50)){
+//     console.error("El numero", numberRandom8, "esta entre el rango de 20 y 50")
+// }
+// else{
+//     console.warn("El numero", numberRandom8, "esta fuera del rango de 20 y 50")
+// }
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 10");
+// Ejercicio 10
+// let numberRandom9 = (Math.floor(Math.random() * (5 + 5 + 1) - 5))
+// if (numberRandom9 != 0) {
+//     console.log("El numero:", numberRandom9, "no es cero")
+// }
+// else {
+//     console.log("El numero", numberRandom9, "es cero")
+// }
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 11");
+// Ejercicio 11
+// let numberRandom10 = Math.floor(Math.random()*(20 -1 +1)+1)
+// if((numberRandom10>=-10) && (numberRandom10<=10)){
+//     console.log("El numero", numberRandom10, "esta dentro del rango de -10 y 10")
+// }
+// else{
+//     console.debug("El numero", numberRandom10, "esta fuera del rango de -10 y 10")
+// }
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 12");
+// Ejercicio 12
+// let numberRandom11 = Math.floor(Math.random()*(2024-1+1)+1)
+// if ((numberRandom11%4==0 && numberRandom11%100 != 0) ||(numberRandom11%400==0)){
+//     console.log("El año", numberRandom11, "es bisiesto")
+// }
+// else{
+//     console.log("El año", numberRandom11, "no es bisiesto")
+// }
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 13");
+// Ejercicio 13
+// const edadUsuario = Number(prompt("Ingrese su edad numerica"))
+// if (edadUsuario>=18){
+//     console.log("Usted es mayor de edad puede acceder")
+// }
+// else{
+//     console.error("Usted es menor de edad no puede acceder")
+// }
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 14");
+// Ejercicio 14
+// console.log("no hay nada en este ejercicio por lo que no se hace nada :)")
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 15");
+// Ejercicio 15
+// let numberRandom12 = Math.floor(Math.random()*(100-1+1)+1)
+// if (Number.isInteger(Math.sqrt(numberRandom12,2))==true){
+// console.log("El numero", numberRandom12, "es un cuadrado perfecto")
+// }
+// else{
+//     console.warn("El numero", numberRandom12, "no es un cuadrado perfecto")
+// }
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 16");
+// Ejercicio 16
+
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 17");
+// Ejercicio 17
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 18");
+// Ejercicio 18
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 19");
+// Ejercicio 19
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 20");
+// Ejercicio 20
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 21");
+// Ejercicio 21
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 22");
+// Ejercicio 22
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 23");
+// Ejercicio 23
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 24");
+// Ejercicio 24
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 25");
+// Ejercicio 25
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 26");
+// Ejercicio 26
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 27");
+// Ejercicio 27
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 28");
+// Ejercicio 28
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 29");
+// Ejercicio 29
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 30");
+// Ejercicio 30
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 31");
+// Ejercicio 31
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 32");
+// Ejercicio 32
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 33");
+// Ejercicio 33
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 34");
+// Ejercicio 34
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 35");
+// Ejercicio 35
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 36");
+// Ejercicio 36
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 37");
+// Ejercicio 37
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 38");
+// Ejercicio 38
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 39");
+// Ejercicio 39
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 40");
+// Ejercicio 40
+console.groupEnd();
