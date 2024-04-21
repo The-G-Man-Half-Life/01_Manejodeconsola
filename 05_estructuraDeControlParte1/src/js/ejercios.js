9ad4bd643d559f3cfea81bbe83e6dc3ec9fad134
The-G-Man-Half-Life <mateomoospina@gmail.com>
2024-04-21 18:33:17 -0500

    trabajo terminado

--- 05_estructuraDeControlParte1/src/js/ejercios.js
+++ 05_estructuraDeControlParte1/src/js/ejercios.js
@@ -165,101 +165,430 @@ console.groupEnd();
 
 console.groupCollapsed("Ejercicio 16");
 // Ejercicio 16
+// let secuenciaFibonacci = [0, 1];
 
+// for (let i = 2; i < 100; i++) {
+//     let siguienteNumero = secuenciaFibonacci[i - 1] + secuenciaFibonacci[i - 2];
+//     secuenciaFibonacci.push(siguienteNumero);
+// }
+
+// let numeroInput = Number(prompt("Ingrese un numero para verificar si es parte de la secuencia de fibonacci"))
+
+// if (secuenciaFibonacci.includes(numeroInput)){
+//     console.log("El numero",numeroInput,"esta dentro de la secuencia fibonacci")
+// }
+// else{
+//     console.error("El numero", numeroInput, "no esta dentro de la secuencia fibonacci")
+// }
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 17");
 // Ejercicio 17
+// let numberRandom13 = Math.floor(Math.random()*(200-1+1)+1)
+
+// let numberVerify = numberRandom13
+
+// while(Number.isInteger(numberVerify)==true){
+//     numberVerify /=2
+//     if (numberVerify==1){
+//         break
+//     }
+// }
+// if (numberVerify==1){
+//     console.log("El numero",numberRandom13,"es un numero potencia de 2")
+// }
+// else{
+//     console.error("El numero", numberRandom13,"no es un numero potencia de 2")
+// }
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 18");
 // Ejercicio 18
+// let numero = String(prompt("Ingrese un numero natural para verificar si es palindromo"))
+
+// let numeroInvertido = (numero.split("").reverse().join(""))
+
+// if (numero==numeroInvertido){
+//     console.log("El numero", numero, "es un numero palindromo")
+// }
+// else{
+//     console.error("El numero", numero, "no es un numero palindromo")
+// }
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 19");
 // Ejercicio 19
+// let palabraInput = String(prompt("Ingrese una cadena de texto cualquiera"))
+
+// let palabraInputVerificada = palabraInput.replace(/[,.;:]/g,"").replace(" ","").toLowerCase()
+
+// if (palabraInputVerificada.search("javascript")>=0){
+//     console.log("La palabra Java Script se encuentra dentro de la frase:",palabraInput)
+// }
+// else{
+//     console.error("La palabra Java Script no se encuentra dentro de la frase: ",palabraInput)
+// }
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 20");
 // Ejercicio 20
+// let palabraInput2 = String(prompt("Ingrese una frase cualquiera"))
+
+// let palabraInputAlt = palabraInput2.length
+
+// if (palabraInputAlt<10){
+//     console.warn("La frase:",palabraInput2, "tiene menos de 10 caracteres")
+// } 
+// else if(palabraInputAlt==10){
+//     console.log("La frase:",palabraInput2, "tiene 10 caracteres")
+// }
+// else{
+//     console.error("La frase:", palabraInput2, "tiene más de 10 caracteres")
+// }
+
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 21");
 // Ejercicio 21
+// let palabraInput3 = String(prompt("Ingrese una cadena de texto cualquiera"))
+// if (palabraInput3.search("A","Á","B","C","D","E","É","F","G","H","I","Í","J","K","L","M","N","O","Ó","P","Q","R","S","T","U","Ú","V","W","X","Y","Z")!=-1){
+//     console.warn("La palabra",palabraInput3 ,"tiene caracteres en mayuscula fatal")
+// }
+// else{
+//     console.log("La palabra",palabraInput3,"no tiene caracteres en mayuscula")
+// }
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 22");
 // Ejercicio 22
+// let palabraInput4 = String(prompt("Ingrese una cadena de texto cualquiera"))
+
+// if (palabraInput4.search("1","2","3","4","5","6","7","8","9","0")>=0){
+//     console.error("La palabra",palabraInput4 ,"tiene numeros. Terrible")
+// }
+// else{
+//     console.log("La palabra",palabraInput4,"no tiene numeros")
+// }
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 23");
 // Ejercicio 23
+// let palabraInput5 = String(prompt("Ingrese una cadena de texto cualquiera"))
+// console.log(palabraInput5.charAt("."))
+// if (palabraInput5.search(/\./)>=0){
+//     console.error("La palabra",palabraInput5 ,"tiene un punto. Terrible")
+// }
+// else{
+//     console.log("La palabra",palabraInput5,"no tiene puntos")
+// }
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 24");
 // Ejercicio 24
+// let palabraInput6 = String(prompt("Ingrese una frase aleatoria"))
+
+// let palabraInput6Alt = palabraInput6.toLowerCase()
+
+// let verificaciona = palabraInput6Alt.search('a')
+// let verificacionb = palabraInput6Alt.search('b')
+// let verificacionc = palabraInput6Alt.search('c')
+// let verificaciond = palabraInput6Alt.search('d')
+// let verificacione = palabraInput6Alt.search('e')
+// let verificacionf = palabraInput6Alt.search('f')
+// let verificaciong = palabraInput6Alt.search('g')
+// let verificacionh = palabraInput6Alt.search('h')
+// let verificacioni = palabraInput6Alt.search('i')
+// let verificacionj = palabraInput6Alt.search('j')
+// let verificacionk = palabraInput6Alt.search('k')
+// let verificacionl = palabraInput6Alt.search('l')
+// let verificacionm = palabraInput6Alt.search('m')
+// let verificacionn = palabraInput6Alt.search('n')
+// let verificaciono = palabraInput6Alt.search('o')
+// let verificacionp = palabraInput6Alt.search('p')
+// let verificacionq = palabraInput6Alt.search('q')
+// let verificacionr = palabraInput6Alt.search('r')
+// let verificacions = palabraInput6Alt.search('s')
+// let verificaciont = palabraInput6Alt.search('t')
+// let verificacionu = palabraInput6Alt.search('u')
+// let verificacionv = palabraInput6Alt.search('v')
+// let verificacionw = palabraInput6Alt.search('w')
+// let verificacionx = palabraInput6Alt.search('x')
+// let verificaciony = palabraInput6Alt.search('y')
+// let verificacionz = palabraInput6Alt.search('z')
+
+// if ((verificaciona>=0)&&(verificacionb>=0)&&(verificacionc>=0)&&(verificaciond>=0)&&(verificacione>=0)&&(verificacionf>=0)&&(verificaciong>=0)&&(verificacionh>=0)&&(verificacioni>=0)&&(verificacionj>=0)&&(verificacionk>=0)&&(verificacionl>=0)&&(verificacionm>=0)&&(verificacionn>=0)&&(verificaciono>=0)&&(verificacionp>=0)&&(verificacionq>=0)&&(verificacionr>=0)&&(verificacions>=0)&&(verificaciont>=0)&&(verificacionu>=0)&&(verificacionv>=0)&&(verificacionw>=0)&&(verificacionx>=0)&&(verificaciony>=0)&&(verificacionz>=0)){
+//     console.log("La frase:",palabraInput6,"tiene todas las letras del alfabeto")
+// }
+// else{
+//     console.error("La frase:",palabraInput6,"no tiene todas las letras del alfabeto")
+// }
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 25");
 // Ejercicio 25
+// let palabraInput7 = String(prompt("Ingrese un dia de la semana cualquiera"))
+// let palabraInput7Alt = palabraInput7.replace(/ /g,"").replace(/[,;.:!]/g,"").toLowerCase()
+
+// let verificacionLunes = palabraInput7Alt.includes("lunes")
+// let verificacionMartes = palabraInput7Alt.includes("martes")
+// let verificacionMiercoles = palabraInput7Alt.includes("miercoles")
+// let verificacionJueves = palabraInput7Alt.includes("jueves")
+// let verificacionViernes = palabraInput7Alt.includes("viernes")
+// let verificacionSabado = palabraInput7Alt.includes("sabado")
+// let verificacionDomingo = palabraInput7Alt.includes("domingo")
+
+// if ((verificacionLunes==true)||(verificacionMartes==true)||(verificacionMiercoles==true)||(verificacionJueves==true)||(verificacionViernes==true)){
+//     console.log("El dia", palabraInput7Alt, "es un dia laboral")
+// }
+// else if ((verificacionSabado==true)||(verificacionDomingo==true)){
+//     console.log("El dia", palabraInput7Alt, "es un dia no laboral")
+// }
+// else{
+//     console.error("LO QUE INGRESASTE NO CORRESPONDE A UN DIA DE LA SEMANA TONOTO")
+// }
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 26");
 // Ejercicio 26
+// let palabraInput8 = String(prompt("Ingrese un dia de la semana cualquiera"))
+// let palabraInput8Alt = palabraInput7.replace(/ /g,"").replace(/[,;.:!]/g,"").toLowerCase()
+
+// let verificacionLunes = palabraInput8Alt.includes("lunes")
+// let verificacionMartes = palabraInput8Alt.includes("martes")
+// let verificacionMiercoles = palabraInput8Alt.includes("miercoles")
+// let verificacionJueves = palabraInput8Alt.includes("jueves")
+// let verificacionViernes = palabraInput8Alt.includes("viernes")
+// let verificacionSabado = palabraInput8Alt.includes("sabado")
+// let verificacionDomingo = palabraInput8Alt.includes("domingo")
+
+// if ((verificacionLunes==true)||(verificacionMartes==true)||(verificacionMiercoles==true)||(verificacionJueves==true)||(verificacionViernes==true)){
+//     console.log("El dia", palabraInput8Alt, "es un dia laboral")
+// }
+// else if ((verificacionSabado==true)||(verificacionDomingo==true)){
+//     console.log("El dia", palabraInput8Alt, "es un dia no laboral")
+// }
+// else{
+//     console.error("LO QUE INGRESASTE NO CORRESPONDE A UN DIA DE LA SEMANA TONOTO")
+// }
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 27");
 // Ejercicio 27
+
+// let palabraInput9 = prompt("Ingrese un numero que corresponda a un mes del año")
+// let palabraInput9Alt = Number(palabraInput9)
+
+// let mesesDelAño = {1:"Enero",2:"Febrero",3:"Marzo",4:"Abril",5:"Mayo",6:"junio",7:"Julio",8:"Agosto",9:"Septiembre",10:"Octubre",11:"Noviembre",12:"Diciembre"}
+
+// if (palabraInput9<=12){
+//     console.log("El valor ingresado corresponde al mes:",mesesDelAño[palabraInput9Alt])
+// } 
+// else{
+//     console.error("El valor ingresado no corresponde a ningun mes del año")
+// }
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 28");
 // Ejercicio 28
+// let horaDia = Number(prompt("Ingrese SOLO la hora actual(formato convencional)"))
+
+// let amOPm = String(prompt("Ingrese si la hora es am o pm"))
+// let amOPmAlt = amOPm.replace(" ","").toLowerCase()
+
+// if (((horaDia < 13 && horaDia >= 9) && (amOPmAlt == "am"))||((horaDia<7 && horaDia>=1)&&(amOPmAlt=="pm"))) {
+//     console.log("Las",horaDia,amOPmAlt.toUpperCase(),"se encuentra dentro del rango de las 9 AM y 6 PM")
+// }
+// else{
+//     console.warn("Las",horaDia,amOPmAlt.toUpperCase(),"no se encuentra dentro del rango de las 9 AM y 6 PM")
+// }
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 29");
 // Ejercicio 29
+// let edadInput = Number(prompt("Ingrese su edad actual en numeros"))
+
+// if (edadInput>65){
+//     console.log("Usted es mayor de 65 años")
+// }
+// else if(edadInput==65){
+//     console.log("Usted tiene 65 años exactamente")
+// }
+// else if (edadInput<65 && edadInput>18){
+//     console.log("Usted se encuentra dentro del rango de edad de 65 y 18 años")
+// }
+// else if (edadInput==18){
+//     console.log("Usted tiene 18 años exactamente")
+// }
+// else{
+//     console.log("Usted es menor de 18 años")
+// }
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 30");
 // Ejercicio 30
+// let anguloA = Number(prompt("Ingrese el valor del angulo A "))
+// let anguloB = Number(prompt("Ingrese el valor del angulo B "))
+// let anguloC = Number(prompt("Ingrese el valor del angulo C "))
+
+// if (((anguloA ==60)&&(anguloB==60)&&(anguloC==60))&&(anguloA+anguloB+anguloC ==180)){
+//     console.log("El triangulo es equilatero")
+// }
+// else if (anguloA+anguloB+anguloC !=180){
+//     console.error("Los valores que ingreso no corresponden a los de un triangulo")
+// }
+// else{
+//     console.log("El triangulo no es equilatero")
+// }
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 31");
 // Ejercicio 31
+// let anguloA1 = Number(prompt("Ingrese el valor del angulo A "))
+// let anguloB1 = Number(prompt("Ingrese el valor del angulo B "))
+// let anguloC1 = Number(prompt("Ingrese el valor del angulo C "))
+
+// if (((anguloA1==anguloB1)||(anguloB1==anguloC1)||(anguloC1==anguloA1))&&(anguloA1+anguloB1+anguloC1 ==180)){
+//     console.log("El triangulo es isosceles")
+// }
+// else if (anguloA1+anguloB1+anguloC1 !=180){
+//     console.error("Los valores que ingreso no corresponden a los de un triangulo")
+// }
+// else{
+//     console.log("El triangulo no es isosceles")
+// }
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 32");
 // Ejercicio 32
+// let anguloA2 = Number(prompt("Ingrese el valor del angulo A "))
+// let anguloB2 = Number(prompt("Ingrese el valor del angulo B "))
+// let anguloC2 = Number(prompt("Ingrese el valor del angulo C "))
+
+// if (((anguloA2!=anguloB2)&&(anguloB2!=anguloC2)&&(anguloC2!=anguloA2))&&(anguloA2+anguloB2+anguloC2 ==180)){
+//     console.log("El triangulo es escaleno")
+// }
+// else if (anguloA2+anguloB2+anguloC2 !=180){
+//     console.error("Los valores que ingreso no corresponden a los de un triangulo")
+// }
+// else{
+//     console.log("El triangulo no es escaleno")
+// }
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 33");
 // Ejercicio 33
+// let numero1 = Number(prompt("Ingrese un numero para verificar si es el doble del siguiente"))
+// let numero2 = Number(prompt("Ingrese el siguiente numero para la prueba"))
+
+// let verificacion = (numero1*2)<numero2
+
+// if (verificacion == true){
+//     console.log("El numero", numero2, "es mayor que el doble del",numero1)
+// }
+// else{
+//     console.error("El numero", numero2, "es menor que el doble del",numero1)
+// }
+
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 34");
 // Ejercicio 34
+// let numero3 = Number(prompt("Ingrese el primer numero"))
+// let numero4 = Number(prompt("Ingrese el segundo numero"))
+
+// let numeroVerificacion = (numero3+numero4)>100
+
+// if (numeroVerificacion == true){
+//     console.log("la suma de los numeros",numero3,"y",numero4,"son mayores que 100")
+// }
+// else{
+//     console.warn("la suma de los numeros",numero3,"y",numero4,"son menores que 100")
+// }
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 35");
 // Ejercicio 35
+// let numero5 = Number(prompt("Ingrese un numero para la prueba"))
+// let numero6 = Number(prompt("Ingrese otro numero"))
+
+// let prueba = (numero5-numero6)<50
+
+// if (prueba==true){
+//     console.log("La resta de",numero5,"y",numero6,"son menores que 50")
+// }
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 36");
 // Ejercicio 36
+// let numero6 = Number(prompt("Ingrese el primer numero"))
+// let numero7 = Number(prompt("Ingrese el segundo numero"))
+
+// let numeroVerificacion1 = (numero6*2)==numero7
+
+// if (numeroVerificacion1 == true){
+//     console.log("El doble del", numero6, "es igual a",numero7)
+// }
+// else{
+//     console.warn("El doble del", numero6, "es distinto a", numero7 )
+// }
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 37");
 // Ejercicio 37
+// let numero8 = Number(prompt("Ingrese el primer numero"))
+// let numero9 = Number(prompt("Ingrese el segundo numero"))
+
+// let numeroVerificacion2 = (numero8*2)==(numero9*3)
+
+// if (numeroVerificacion2 == true){
+//     console.log("El doble del", numero8, "es igual al triple del",numero9)
+// }
+// else{
+//     console.warn("El doble del", numero8, "no es igual al triple del",numero9)
+// }
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 38");
 // Ejercicio 38
+// let numero10 = Number(prompt("Ingrese un numero para la prueba"))
+// let numero11 = Number(prompt("Ingrese otro numero"))
+
+// let prueba = (numero10+numero11)==50
+
+// if (prueba==true){
+//     console.log("La suma de los numeros", numero10,"y",numero11,"es igual a 50")
+// }
+// else{
+//     console.error("La suma de los numeros",numero10,"y",numero11,"no es igual a 50")
+// }
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 39");
 // Ejercicio 39
+// let numero11 = Number(prompt("Ingrese un numero para la prueba"))
+// let numero12 = Number(prompt("Ingrese otro numero"))
+
+// let prueba = (numero11*numero12)>500
+
+// if (prueba==true){
+//     console.log("La multiplicacion de los numeros",numero11,"y",numero12,"son mayores que 500")
+// }
+// else{
+//     console.error("La multiplicacion de los numeros",numero11,"y",numero12,"es menor que 500")
+// }
 console.groupEnd();
 
 console.groupCollapsed("Ejercicio 40");
 // Ejercicio 40
+// let numero13 = Number(prompt("Ingrese un numero para la prueba"))
+// let numero14 = Number(prompt("Ingrese otro numero"))
+
+// let prueba = (numero13/numero14)<10
+
+// if (prueba==true){
+//     console.log("La division de los numeros",numero13,"y",numero14,"es menor que 10")
+// }
+// else{
+//     console.error("La multiplicacion de los numeros",numero13,"y",numero14,"es mayor que 10")
+// }
 console.groupEnd();
