f0978020f2761764547507abf28dfb20cdc4d5e2
The-G-Man-Half-Life <mateomoospina@gmail.com>
2024-04-22 14:33:59 -0500

    trabajo avanzado

---
+++ 06_estructuraDeControlParte1/ejercicios/src/js/index.js
@@ -0,0 +1,136 @@
+console.groupCollapsed("Ejercicio 1");
+// Ejercicio 1
+// const numero1 = Number(prompt("Ingrese un numero cualquiera"))
+// const numero2 = Number(prompt("Ingrese otro numero cualquiera"))
+
+// let comparacionValores = undefined
+
+// let operacionCaso1 = (numero1) - (numero2)
+// let operacionCaso2 = (numero2) - (numero1)
+
+// if (numero1 > numero2) {
+//     comparacionValores = true
+// }
+// else {
+//     comparacionValores = false
+// }
+
+// if (comparacionValores == true) {
+//     console.log(`El resultado de la operacion es ${numero1}-${numero2}: ${operacionCaso1}`)
+//     if (operacionCaso1 < 0) {
+//         console.log("El resultado de la operacion es negativo")
+//     }
+//     else {
+//         console.log("El resultado de la operacion es positiva")
+//     }
+// }
+// else {
+//     console.log(`El resultado de la operacion es ${numero2}-${numero1}: ${operacionCaso2}`)
+//     if (operacionCaso2 < 0) {
+//         console.log("El resultado de la operacion es negativo")
+//     }
+//     else {
+//         console.log("El resultado de la operacion es positiva")
+//     }
+// }
+
+
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 2");
+// Ejercicio 2
+// let opcion = prompt(`[Ingrese el numero o el nombre de las siguientes operaciones]
+
+// 1.Suma
+// 2.Resta
+// 3.Multiplicacion
+// 4.Division
+// 5.Potencia
+// 6.Raiz`)
+// let numero3 = Number(prompt("Ingrese un numero cualquiera"))
+// let numero4 = Number(prompt("Ingrese otro numero cualquiera"))
+
+
+// let opcionSwitch = undefined
+
+// if (opcion == "1" || opcion == "2" || opcion == "3" || opcion == "3" || opcion == "4" || opcion == "5" || opcion == "6") {
+//     opcionSwitch = Number(opcion)
+// }
+// else {
+//     opcionSwitch = opcion.toLowerCase()
+// }
+
+// switch (opcionSwitch) {
+//     case 1:
+//         console.log(`Su resultado de la suma de ${numero3} y ${numero4}:${Number.parseFloat(Math.round(numero3 + numero4))}`)
+//         break;
+//     case "suma":
+//         console.log(`Su resultado de la suma de ${numero3} y ${numero4}:${Number.parseFloat(Math.round(numero3 + numero4))}`)
+//         break;
+//     case 2:
+//         console.log(`Su resultado de la resta de ${numero3} y ${numero4}:${Number.parseFloat(Math.round(numero3 - numero4))}`)
+//         break;
+//     case "resta":
+//         console.log(`Su resultado de la resta de ${numero3} y ${numero4}:${Number.parseFloat(Math.round(numero3 - numero4))}`)
+//         break;
+//     case 3:
+//         console.log(`Su resultado de la multiplicacion de ${numero3} y ${numero4}:${Number.parseFloat(Math.round(numero3 * numero4))}`)
+//         break;
+//     case "multiplicacion":
+//         console.log(`Su resultado de la multiplicacion de ${numero3} y ${numero4}:${Number.parseFloat(Math.round(numero3 * numero4))}`)
+//         break;
+//     case 4:
+//         console.log(`Su resultado de la division de ${numero3} y ${numero4}:${Number.parseFloat(Math.round(numero3 / numero4))}`)
+//         break;
+//     case "division":
+//         console.log(`Su resultado de la division de ${numero3} y ${numero4}:${Number.parseFloat(Math.round(numero3 / numero4))}`)
+//         break;
+//     case 5:
+//         console.log(`Su resultado de la potencia de ${numero3} y ${numero4}:${Number.parseFloat(Math.round(Math.pow(numero3, numero4)))}`)
+//         break;
+//     case "potencia":
+//         console.log(`Su resultado de la potencia de ${numero3} y ${numero4}:${Number.parseFloat(Math.round(Math.pow(numero3, numero4)))}`)
+//         break;
+//     case 6:
+//         console.log(`Su resultado de la raiz de ${numero3} y ${numero4}:${Number.parseFloat(Math.round(Math.sqrt(numero3, numero4)))}`);
+//         break;
+//     case "raiz":
+//         console.log(`Su resultado de la raiz de ${numero3} y ${numero4}:${Number.parseFloat(Math.round(Math.sqrt(numero3, numero4)))}`);
+//         break;
+//     default:
+//         break;
+// }
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 3");
+// Ejercicio 3
+let numeroMultiplicarTabla = Number(prompt("Ingrese un numero para saber su tabla de multiplicar"))
+
+for (let i = 1; i<10;i++){
+    console.log(`${i}.${numeroMultiplicarTabla} veces ${i} es igual a:`,toString(i*numeroMultiplicarTabla))
+}
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 4");
+// Ejercicio 4
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 5");
+// Ejercicio 5
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 6");
+// Ejercicio 6
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 7");
+// Ejercicio 7
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 8");
+// Ejercicio 8
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 9");
+// Ejercicio 9
+console.groupEnd();
